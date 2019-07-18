9f3eaf9aafd70c410211b3983e01b409e285246d
Faisal Alam <faisal.11616526@lpu.in>
2019-07-19 02:52:02 +0530

    Initial commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd $(dirname "$0")
+
+DIR=$(cd -)
+
+for lang in $(ls "$DIR/containers")
+do
+    cd ${DIR}/containers/${lang}
+    docker image rm ifaisalalam/ide-worker-${lang}
+    docker build -t ifaisalalam/ide-worker-${lang}
+    cd ${DIR}
+done
---
+++ tests/cpp/test.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+cd $(dirname "$0")
+
+DIR=$(pwd)
+
+RUN_BOX="${DIR}/box"
+
+echo ${RUN_BOX};
+
+mkdir -p ${RUN_BOX}
+
+cp -fv ${DIR}/source.cpp ${RUN_BOX}/source.cpp
+cp -fv ${DIR}/run.stdin ${RUN_BOX}/run.stdin
+
+docker run \
+    --cpus="0.5" \
+    --memory="50m" \
+    --ulimit nofile=64:64 \
+    --rm \
+    --read-only \
+    -v "$RUN_BOX":/var/box \
+    -w /var/box \
+    ifaisalalam/ide-worker-cpp \
+    bash -c "/bin/compile.sh && /bin/run.sh"
+
+ls -lh ${RUN_BOX}
+
+expected="Hello world"
+actual="$(cat ${RUN_BOX}/run.stdout)"
+
+rm -rf ${RUN_BOX}
+
+if [[ "$expected" == "$actual" ]]; then
+    echo "$DIR : TEST SUCCESS"
+else
+    echo "MISMATCH: Expected = $expected; Actual = $actual"
+    exit 1
+fi
