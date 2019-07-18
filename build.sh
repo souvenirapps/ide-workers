d47918c99419baa1c5aa55c7de6cc23653d00f81
Faisal Alam <faisal.11616526@lpu.in>
2019-07-19 03:29:40 +0530

    Debug Travis CI: Print current directory while building docker images

--- build.sh
+++ build.sh
@@ -7,7 +7,10 @@ DIR=$(cd -)
 for lang in $(ls "$DIR/containers")
 do
     cd ${DIR}/containers/${lang}
-    ls ${DIR}
+
+    echo "CURRENT DIR: ${DIR}/containers/${lang}"
+    ls ${DIR}/containers/${lang}
+    
     docker image rm ifaisalalam/ide-worker-${lang}
     docker build -t ifaisalalam/ide-worker-${lang}
     cd ${DIR}
