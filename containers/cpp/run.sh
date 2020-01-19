ae0527cb596957bdfe969638ee5cf9589e32da73
Faisal Alam <faisal.alam@rentomojo.com>
2020-01-19 17:27:30 +0530

    Add timeout on code execution
    Timeout code execution after certain seconds if the code does not finish execution automatically, and trigger a TLE error.
    
    The timeout duration (in seconds) could be set through passing environment variable `$DEFAULT_TIMEOUT` to the docker container. The default timeout is set to 5 seconds.

--- containers/cpp/run.sh
+++ containers/cpp/run.sh
@@ -2,4 +2,8 @@
 
 chmod 755 exe
 
-./exe < run.stdin 2> run.stderr 1> run.stdout
+if [ -z "$DEFAULT_TIMEOUT" ]; then
+  export DEFAULT_TIMEOUT=5
+fi
+
+timeout $DEFAULT_TIMEOUT ./exe < run.stdin 2> run.stderr 1> run.stdout || echo "TLE: Execution exceeded the maximum timelimit." > run.stderr
