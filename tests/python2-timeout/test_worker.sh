cd0670ecaa803c16af1f45abe766910917b54297
Faisal Alam <faisal.alam@rentomojo.com>
2020-01-29 19:08:44 +0530

    Fix `python2-timeout` test

--- tests/python2-timeout/test_worker.sh
+++ tests/python2-timeout/test_worker.sh
@@ -10,7 +10,7 @@ echo "${RUN_BOX}";
 
 mkdir -p "${RUN_BOX}"
 
-cp -fv "${DIR}/source.cpp" "${RUN_BOX}/source.cpp"
+cp -fv "${DIR}/source.py" "${RUN_BOX}/source.py"
 echo "" > "${RUN_BOX}/run.stdin"
 
 docker run \
