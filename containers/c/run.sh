fa0df3ce84851256f6de691c0cb795da4c3874db
Faisal Alam <faisal.alam@rentomojo.com>
2020-01-29 18:03:33 +0530

    Update container runtime scripts
    Save TLE error to `tle.stderr` file.

--- containers/c/run.sh
+++ containers/c/run.sh
@@ -6,4 +6,4 @@ if [ -z "$DEFAULT_TIMEOUT" ]; then
   export DEFAULT_TIMEOUT=5
 fi
 
-timeout $DEFAULT_TIMEOUT ./exe < run.stdin 2> run.stderr 1> run.stdout || echo "TLE: Execution exceeded the maximum timelimit." > run.stderr
+timeout $DEFAULT_TIMEOUT ./exe < run.stdin 2> run.stderr 1> run.stdout || echo "TLE: Execution exceeded the maximum timelimit." > tle.stderr
